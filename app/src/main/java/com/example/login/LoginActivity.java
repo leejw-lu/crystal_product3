1fbd08b6a9494add3b55e33d185b904631a34023
leejw-lu <jiwoojenny@naver.com>
2021-07-15 16:23:35 +0900

    로그인/회원가입 구현완료

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -16,7 +16,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+//
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth nFirebaseAuth;     //firebase인증
