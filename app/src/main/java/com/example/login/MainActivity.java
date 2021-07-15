5921648e3d569b554acd7bb96f5af0439c38094f
leejw-lu <jiwoojenny@naver.com>
2021-07-15 17:44:56 +0900

    login 후 main화면으로

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView view = (RecyclerView)findViewById(R.id.main_recyclerview);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(new MyRecyclerViewAdapter());
     }
 }
