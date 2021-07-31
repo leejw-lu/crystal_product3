2f3a16036052ccb2974857dee987ae5be53f13a0
zzhxxnee <20201008@sungshin.ac.kr>
2021-07-31 17:50:36 +0900

    카드뷰 + 홈화면 + firebase 카드뷰 디자인 수정

--- app/src/main/java/com/example/login/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/login/MyRecyclerViewAdapter.java
@@ -12,7 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.storage.FirebaseStorage;
 
 import org.jetbrains.annotations.NotNull;
@@ -57,10 +62,11 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         return new ViewHolder(view);
     }
 
+
     @Override
     public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
         //holder.textViewUser.setText(imageDTOList.get(position).getUserId()); //fragment라 그런지 ViewHolder안하면 오류남.
-        ((ViewHolder)holder).textViewUser.setText(imageDTOList.get(position).getUserId());
+        //((ViewHolder)holder).textViewUser.setText(imageDTOList.get(position).getUserId());
         ((ViewHolder)holder).textViewTitle.setText(imageDTOList.get(position).getTitle());
         ((ViewHolder)holder).textViewDesc.setText(imageDTOList.get(position).getDescription());
         ((ViewHolder)holder).imageViewHeart.setImageResource(R.drawable.heart_off);
@@ -83,7 +89,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     //ViewHolder 클래스
     class ViewHolder extends RecyclerView.ViewHolder
     {
-        public TextView textViewUser;
+        //public TextView textViewUser;
         public TextView textViewTitle;
         public TextView textViewDesc;
         public ImageView imageView;
@@ -92,7 +98,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         public ViewHolder(@NonNull View itemView)
         {
             super(itemView);
-            textViewUser = itemView.findViewById(R.id.item_user);
+            //textViewUser = itemView.findViewById(R.id.item_user);
             textViewTitle = itemView.findViewById(R.id.item_title); //파라메타 id 찾기
             textViewDesc = itemView.findViewById(R.id.item_desc);
             imageView = itemView.findViewById(R.id.item_image);
@@ -101,98 +107,97 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     }
 
 
-    /*------기본 item3개 세팅하기.
-    public MyRecyclerViewAdapter() {
-        cardViewItemDTOS.add(new CardViewItemDTO(R.drawable.snowball,"스노우볼","수요조사 진행 중"));
-        cardViewItemDTOS.add(new CardViewItemDTO(R.drawable.mugcup,"머그컵","7,000원"));
-        cardViewItemDTOS.add(new CardViewItemDTO(R.drawable.milkkeyring,"우유키링","7,500원"));
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        //XML 세팅
-
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item,parent,false);
-
-        return new RowCell(view);
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        database = FirebaseDatabase.getInstance();
-        ((RowCell)holder).imageView.setImageResource(cardViewItemDTOS.get(position).imageview);
-        ((RowCell)holder).title.setText(cardViewItemDTOS.get(position).title);
-        ((RowCell)holder).subtitle.setText(cardViewItemDTOS.get(position).subtitle);
-        //아이템 세팅
-
-        ((RowCell) holder).heartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                onStarClicked(database.getReference().child("images").child(uidLists.get(position)));
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        //이미지 카운터
-        return cardViewItemDTOS.size();
-    }
-
-    private void onStarClicked(DatabaseReference postRef) {
-        auth = FirebaseAuth.getInstance();
-        postRef.runTransaction(new Transaction.Handler() {
-            @Override
-            public Transaction.Result doTransaction(MutableData mutableData) {
-                CardViewItemDTO cardViewItemDTO = mutableData.getValue(CardViewItemDTO.class);
-                if (cardViewItemDTO == null) {
-                    return Transaction.success(mutableData);
-                }
-
-                if (cardViewItemDTO.stars.containsKey(auth.getCurrentUser().getUid())) {
-                    // Unstar the post and remove self from stars
-                    cardViewItemDTO.starCount = cardViewItemDTO.starCount - 1;
-                    cardViewItemDTO.stars.remove(auth.getCurrentUser().getUid());
-                } else {
-                    // Star the post and add self to stars
-                    cardViewItemDTO.starCount = cardViewItemDTO.starCount + 1;
-                    cardViewItemDTO.stars.put(auth.getCurrentUser().getUid(), true);
-                }
-
-                // Set value and report transaction success
-                mutableData.setValue(cardViewItemDTO);
-                return Transaction.success(mutableData);
-            }
-
-            @Override
-            public void onComplete(DatabaseError error, boolean committed, DataSnapshot currentData) {
-                // Transaction completed
-
-            }
-
-        });
-    }
-
-
-    private static class RowCell extends RecyclerView.ViewHolder {
-        public ImageView imageView;
-        public TextView title;
-        public TextView subtitle;
-        ImageView heartButton;
-
-        public RowCell(View view) {
-            super(view);
-            imageView = (ImageView)view.findViewById(R.id.cardview_imageview);
-            title = (TextView)view.findViewById(R.id.cardview_title);
-            subtitle = (TextView)view.findViewById(R.id.cardview_subtitle);
-            heartButton = (ImageView)view.findViewById(R.id.btn_heart);
-        }
-    }
-    */
-
-
-
+    /////////////  아래로 성원이 코드 ///////////////////////
+    /*------기본 item3개 세팅하기.*/
+//    public MyRecyclerViewAdapter() {
+//        cardViewItemDTOS.add(new CardViewItemDTO(R.drawable.snowball,"스노우볼","수요조사 진행 중"));
+//        cardViewItemDTOS.add(new CardViewItemDTO(R.drawable.mugcup,"머그컵","7,000원"));
+//        cardViewItemDTOS.add(new CardViewItemDTO(R.drawable.milkkeyring,"우유키링","7,500원"));
+//    }
+//
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        //XML 세팅
+//
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item,parent,false);
+//
+//        return new RowCell(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//        database = FirebaseDatabase.getInstance();
+//        ((RowCell)holder).imageView.setImageResource(cardViewItemDTOS.get(position).imageview);
+//        ((RowCell)holder).title.setText(cardViewItemDTOS.get(position).title);
+//        ((RowCell)holder).subtitle.setText(cardViewItemDTOS.get(position).subtitle);
+//        //아이템 세팅
+//
+//        ((RowCell) holder).heartButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                onStarClicked(database.getReference().child("images").child(uidLists.get(position)));
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        //이미지 카운터
+//        return cardViewItemDTOS.size();
+//    }
+//
+//    private void onStarClicked(DatabaseReference postRef) {
+//        auth = FirebaseAuth.getInstance();
+//        postRef.runTransaction(new Transaction.Handler() {
+//            @Override
+//            public Transaction.Result doTransaction(MutableData mutableData) {
+//                CardViewItemDTO cardViewItemDTO = mutableData.getValue(CardViewItemDTO.class);
+//                if (cardViewItemDTO == null) {
+//                    return Transaction.success(mutableData);
+//                }
+//
+//                if (cardViewItemDTO.stars.containsKey(auth.getCurrentUser().getUid())) {
+//                    // Unstar the post and remove self from stars
+//                    cardViewItemDTO.starCount = cardViewItemDTO.starCount - 1;
+//                    cardViewItemDTO.stars.remove(auth.getCurrentUser().getUid());
+//                } else {
+//                    // Star the post and add self to stars
+//                    cardViewItemDTO.starCount = cardViewItemDTO.starCount + 1;
+//                    cardViewItemDTO.stars.put(auth.getCurrentUser().getUid(), true);
+//                }
+//
+//                // Set value and report transaction success
+//                mutableData.setValue(cardViewItemDTO);
+//                return Transaction.success(mutableData);
+//            }
+//
+//            @Override
+//            public void onComplete(DatabaseError error, boolean committed, DataSnapshot currentData) {
+//                // Transaction completed
+//
+//            }
+//
+//        });
+//    }
+//
+//
+//    private static class RowCell extends RecyclerView.ViewHolder {
+//        public ImageView imageView;
+//        public TextView title;
+//        public TextView subtitle;
+//        ImageView heartButton;
+//
+//        public RowCell(View view) {
+//            super(view);
+//            imageView = (ImageView)view.findViewById(R.id.cardview_imageview);
+//            title = (TextView)view.findViewById(R.id.cardview_title);
+//            subtitle = (TextView)view.findViewById(R.id.cardview_subtitle);
+//            heartButton = (ImageView)view.findViewById(R.id.btn_heart);
+//        }
+//    }
+//
+//
 
 }
 
