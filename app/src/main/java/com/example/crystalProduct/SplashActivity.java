b354ca4ccf83e7a7db52812ec0ad9878760be168
zzhxxnee <20201008@sungshin.ac.kr>
2021-08-12 05:25:47 +0900

    연보라 -> 바이올렛 색으로 변경 & 여러 부가기능 추가

---
+++ app/src/main/java/com/example/crystalProduct/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.crystalProduct;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
+    }
+}
